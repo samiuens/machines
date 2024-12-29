fdbdcab5b30f48431a16eacc67d708e5293f859e
Sami Arda Ünsay <github@samiarda.com>
2024-12-29 17:01:25 +0100

    nixos: add locale & security to system

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -4,8 +4,10 @@
       ./system/bluetooth.nix
       ./system/boot.nix
       ./system/gnome.nix
+      ./system/locale.nix
       ./system/networking.nix
       ./system/print.nix
+      ./system/security.nix
       ./system/sound.nix
 
       ./security.nix
