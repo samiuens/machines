4232d5c1941593959d5ac5c476e23b413d8570ea
Sami Arda Ünsay <github@samiarda.com>
2024-12-29 19:50:53 +0100

    nixos: extra groups for user

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -1,6 +1,7 @@
 { config, ...}: {
   users.users."samiarda" = {
     home = "/home/samiarda";
+    extraGroups = [ "networkmanager" "wheel"];
   };
   home-manager.users."samiarda" = import ./home.nix;
 }
