3565ccd05dfe2ae23de366f192680729a0944272
Sami Arda Ünsay <github@samiarda.com>
2025-01-01 15:21:05 +0100

    nixos: switch bootloader to 'grub'

--- modules/nixos/system/boot.nix
+++ modules/nixos/system/boot.nix
@@ -1,4 +1,11 @@
 { config, ...}: {  
-  boot.loader.systemd-boot.enable = true;
+  #boot.loader.systemd-boot.enable = true;
+  boot.loader.grub = {
+    enable = true;
+    version = 2;
+    device = "nodev";
+    efiSupport = true;
+  };
+  boot.loader.grub.useOSProber = true;
   boot.loader.efi.canTouchEfiVariables = true;
 }
