bb8e3aee2ce5c8221edd11d9442370433c855b73
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-01-14 14:49:52 +0100

    shared: clear ssh hosts

--- modules/shared/programs/ssh.nix
+++ modules/shared/programs/ssh.nix
@@ -2,20 +2,6 @@
   programs.ssh = {
     enable = true;
     forwardAgent = true;
-    matchBlocks = {
-      "github.com" = {
-        forwardAgent = false;
-        identitiesOnly = true;
-        identityFile = "${config.home.homeDirectory}/.ssh/github-29165650";
-        #identityFile = "${config.home.homeDirectory}/.ssh/github-29166669";
-      };
-      "srv-prod-1" = {
-        user = "samiarda";
-        hostname = "10.10.0.55";
-        forwardAgent = false;
-        identitiesOnly = true;
-        identityFile = "${config.home.homeDirectory}/.ssh/srv-prod-1-29166650";
-      };
-    };
+    matchBlocks = {};
   };
 }
