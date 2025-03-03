11ae78d8212d7c35416f450b4f55c95820fabbb5
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-03-03 16:16:27 +0100

    use dynamic username in config

--- darwin/shared/system/finder.nix
+++ darwin/shared/system/finder.nix
@@ -1,11 +1,11 @@
-{ ... }: {
+{ username, ... }: {
   system.defaults = {
     finder = {
       AppleShowAllFiles = false;
       FXDefaultSearchScope = "SCcf";
       FXPreferredViewStyle = "Nlsv";
       NewWindowTarget = "Other";
-      NewWindowTargetPath = "file:///Users/suensay/Nextcloud";
+      NewWindowTargetPath = "file:///Users/${username}/Nextcloud";
       ShowPathbar = true;
       _FXSortFoldersFirst = true;
       _FXSortFoldersFirstOnDesktop = true;
