474c3cc31e869ef4a41753f7a594fe364ee4d713
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-03-03 18:47:54 +0100

    using determinate nix daemon

--- darwin/shared/modules/nix.nix
+++ darwin/shared/modules/nix.nix
@@ -1,21 +1,7 @@
 { username, platform, lib, ... }: {
   nix = {
+    enable = false;
     channel.enable = false;
-    gc = {
-      automatic = true;
-      options = "--delete-older-than 7d";
-      interval = { Weekday = 0; Hour = 0; Minute = 0; };
-    };
-    optimise = {
-      automatic = true;
-      interval = { Weekday = 0; Hour = 0; Minute = 0; };
-    };
-    settings = {
-      allowed-users = [ "root" "${username}" ];
-      trusted-users = [ "root" "${username}" ];
-      experimental-features = "flakes nix-command";
-      warn-dirty = false;
-    };
   };
   nixpkgs = {
     config.allowUnfree = true;
