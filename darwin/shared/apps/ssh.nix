f128e24d83518d38d444ffe6b3ca06fff5c17aa9
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-03-19 12:00:16 +0100

    added 1password ssh agent + example for ssh host

--- darwin/shared/apps/ssh.nix
+++ darwin/shared/apps/ssh.nix
@@ -1,5 +1,17 @@
-{ pkgs, ... }: {
+{ ... }: {
   programs.ssh = {
     enable = true;
+    extraConfig = ''
+      Host *
+        IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+    '';
+    /*matchBlocks = {
+      "srv-prod-1" = {
+        Host = "srv-prod-1";
+        HostName = "srv-prod-1.samiuen.com";
+        User = "samiarda";
+        forwardAgent = false;
+        identityFile = [ "" "" ];
+    }*/
   };
 }
