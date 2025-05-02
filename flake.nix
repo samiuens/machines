60133145814c2a85dcbb41e9c9782898aba04f8e
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-02 12:17:20 +0200

    switched to nixpkgs 'unstable'

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "samiarda's machine configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     darwin = {
       url = "github:LnL7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
