e097e8b5bc58bdbbf3b480c1c119b6845e84a711
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-11 21:33:30 +0200

    dynamic dock setup (try 2)

---
+++ darwin/shared/system/dock/samiarda.nix
@@ -0,0 +1,12 @@
+{ ... }: {
+  system.defaults = {
+    dock = {
+      persistent-apps = [
+        "/Applications/Google Chrome.app"
+        "/Applications/Notion Calendar.app"
+        "/Applications/Cursor.app"
+        "/Applications/GitKraken.app"
+      ];
+    };
+  }
+}
---
+++ darwin/shared/system/dock/samiuensay.nix
@@ -0,0 +1,12 @@
+{ ... }: {
+  system.defaults = {
+    dock = {
+      persistent-apps = [
+        "/Applications/Arc.app"
+        "/Applications/Todoist.app"
+        "/Applications/Obsidian.app"
+        "/Applications/Notion Calendar.app"
+      ];
+    };
+  }
+}
